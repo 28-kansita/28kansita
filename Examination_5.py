7eb256c2829e50f122f8503bf2da7ca082592b47
28-kansita <kansita.bnn14@gmail.com>
2026-09-17 16:25:20 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-hour = int(input())
+hour = int(input("เวลา(0-23) : "))
+if hour < 12:
+    print("Morning")
+elif hour <=17:
+    print("Afternoon")
+else:
+    print("Night")
